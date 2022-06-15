9fef0b3415f221cb82a19fdc07106ac1699f9e96
MuhannedAlogaidi <malogaidi@ttsiglobal.com>
2022-06-15 01:43:58 -0400

    Update lint configure Update lint configure

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const App = () =>{
     return (
         <h1>
-            Hello world! I am using React
+            Hello world! I am using React and tailwind
         </h1>
     )
 }
